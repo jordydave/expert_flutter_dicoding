6450466957d774e6b70d8aafdd29b434f254e634
Jordy <jordydave1@gmail.com>
2023-05-01 08:05:25 +0700

    Refactor: Separate blocs to another class

---
+++ lib/blocs.dart
@@ -0,0 +1,54 @@
+import 'package:ditonton/presentation/bloc/movie/movie_detail_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/movie_genre_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/movie_genre_list_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/movie_watchlist_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/now_playing_movie_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/popular_movie_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/top_rated_movie_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/upcoming_movie_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/now_playing_tv_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/popular_tv_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/search_tv_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/top_rated_tv_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/tv_detail_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/tv_genre_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/tv_genre_list_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv/tv_watchlist_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ditonton/injection.dart' as di;
+
+import 'presentation/bloc/movie/search_bloc.dart';
+
+class BlocProviderManager {
+  static final BlocProviderManager _singleton = BlocProviderManager._internal();
+
+  factory BlocProviderManager() {
+    return _singleton;
+  }
+
+  BlocProviderManager._internal();
+
+  final List<BlocProvider> blocProviders = [
+    BlocProvider(create: (_) => di.locator<SearchBloc>()),
+    BlocProvider(create: (_) => di.locator<SearchTvBloc>()),
+    BlocProvider(create: (_) => di.locator<TopRatedMovieBloc>()),
+    BlocProvider(create: (_) => di.locator<NowPlayingMovieBloc>()),
+    BlocProvider(create: (_) => di.locator<PopularMovieBloc>()),
+    BlocProvider(create: (_) => di.locator<UpcomingMovieBloc>()),
+    BlocProvider(create: (_) => di.locator<NowPlayingTvBloc>()),
+    BlocProvider(create: (_) => di.locator<PopularTvBloc>()),
+    BlocProvider(create: (_) => di.locator<TopRatedTvBloc>()),
+    BlocProvider(create: (_) => di.locator<MovieDetailBloc>()),
+    BlocProvider(create: (_) => di.locator<MovieWatchlistBloc>()),
+    BlocProvider(create: (_) => di.locator<TvDetailBloc>()),
+    BlocProvider(create: (_) => di.locator<TvWatchlistBloc>()),
+    BlocProvider(create: (_) => di.locator<MovieGenreBloc>()),
+    BlocProvider(create: (_) => di.locator<MovieGenreListBloc>()),
+    BlocProvider(create: (_) => di.locator<TvGenreListBloc>()),
+    BlocProvider(create: (_) => di.locator<TvGenreBloc>()),
+  ];
+
+  List<BlocProvider> getBlocProviders() {
+    return blocProviders;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,5 @@
+import 'package:ditonton/blocs.dart';
 import 'package:ditonton/firebase_options.dart';
-import 'package:ditonton/presentation/bloc/movie/movie_detail_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/movie_genre_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/movie_genre_list_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/movie_watchlist_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/now_playing_movie_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/popular_movie_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/search_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/top_rated_movie_bloc.dart';
-import 'package:ditonton/presentation/bloc/movie/upcoming_movie_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/now_playing_tv_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/popular_tv_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/search_tv_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/top_rated_tv_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/tv_detail_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/tv_genre_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/tv_genre_list_bloc.dart';
-import 'package:ditonton/presentation/bloc/tv/tv_watchlist_bloc.dart';
 import 'package:ditonton/presentation/pages/about_page.dart';
 import 'package:ditonton/presentation/pages/movie/home_movie_page.dart';
 import 'package:ditonton/presentation/pages/movie/movie_detail_page.dart';
@@ -59,59 +43,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => di.locator<SearchBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<SearchTvBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<TopRatedMovieBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<NowPlayingMovieBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<PopularMovieBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<UpcomingMovieBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<NowPlayingTvBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<PopularTvBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<TopRatedTvBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<MovieDetailBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<MovieWatchlistBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<TvDetailBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<TvWatchlistBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<MovieGenreBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<MovieGenreListBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<TvGenreListBloc>(),
-        ),
-        BlocProvider(
-          create: (_) => di.locator<TvGenreBloc>(),
-        ),
-      ],
+      providers: BlocProviderManager().getBlocProviders(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'FilmFinder',
